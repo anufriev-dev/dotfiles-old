84b905a5dc1719b61ead862f5e2169c94e03427e
Anufriev Georgy <g@anufriev.info>
2024-10-02 09:37:01 +0500

    m: post;pre commit 2

--- dotfiles/.main/install.sh
+++ dotfiles/.main/install.sh
@@ -3,10 +3,10 @@
 # ~/dotfiles/.main/install.sh
 #
 
-cf='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+GIT='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 
 # set a hooks dir for git (bare config)
-$cf config --local core.hooksPath "$HOME/dotfiles/.main/hooks"
+$GIT config --local core.hooksPath "$HOME/dotfiles/.main/hooks"
 
 # install apps
 if [[ -f package.sh ]]; then
