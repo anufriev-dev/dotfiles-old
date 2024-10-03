115ee2c1fd2043674f712dba41eec7c934964c4a
Anufriev Georgy <g@anufriev.info>
2024-10-03 08:32:38 +0500

    new paths

--- dotfiles/.main/install.sh
+++ dotfiles/.main/install.sh
@@ -3,12 +3,15 @@
 # ~/dotfiles/.main/install.sh
 #
 
-GIT='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+export GIT="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+export EXT_PATH="$HOME/dotfiles/.main/.vscode/extensions.txt"
+export BASE_BRANCH=$($GIT rev-parse --abbrev-ref HEAD)
+
 
 # set a hooks dir for git (bare config)
 $GIT config --local core.hooksPath "$HOME/dotfiles/.main/hooks"
 
 # install apps
 if [[ -f package.sh ]]; then
-    ./package.sh
+    . ./package.sh
 fi
