843be50230f4a53a99677268ea65efd024de744f
Anufriev Georgy <g@anufriev.info>
2024-10-03 08:49:01 +0500

    new env file

--- dotfiles/.main/install.sh
+++ dotfiles/.main/install.sh
@@ -3,10 +3,9 @@
 # ~/dotfiles/.main/install.sh
 #
 
-export GIT="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
-export EXT_PATH="$HOME/dotfiles/.main/.vscode/extensions.txt"
-export BASE_BRANCH=$($GIT rev-parse --abbrev-ref HEAD)
-
+set -a
+. .env
+set +a
 
 # set a hooks dir for git (bare config)
 $GIT config --local core.hooksPath "$HOME/dotfiles/.main/hooks"
